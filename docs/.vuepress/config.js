6d47e5543ec0c6894856e7fe64c982c25b2dca0f
Jason Viloria <jason.viloria@optiscangroup.com>
2020-07-19 02:59:02 +0300

    work in progress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  title: 'Kubernetes and Harbor',
+  description: 'Kubernetes and Harbor',
+  base: '/k8s-harbor/',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    displayAllHeaders: true,
+    lastUpdated: true,
+    repo: 'ruzickap/k8s-harbor',
+    docsDir: 'docs',
+    editLinks: true,
+    logo: '/harbor-horizontal-color.svg',
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'Harbor links',
+        items: [
+          { text: 'Harbor', link: 'https://goharbor.io/' },
+          { text: 'Harbor Blog', link: 'https://goharbor.io/blog/' },
+          { text: 'Harbor Docs', link: 'https://goharbor.io/docs/' },
+          { text: 'Harbor GitHub', link: 'https://github.com/goharbor/harbor/' },
+          { text: 'Harbor Community', link: 'https://goharbor.io/community/' }
+        ]
+      }
+    ],
+    sidebar: [
+      '/',
+      '/part-01/',
+      '/part-02/',
+      '/part-03/',
+      '/part-04/',
+      '/part-05/',
+      '/part-06/',
+      '/part-07/',
+      '/part-08/',
+      '/part-09/'
+    ]
+  },
+  plugins: [
+    ['@vuepress/medium-zoom'],
+    ['@vuepress/back-to-top'],
+    ['reading-progress'],
+    ['smooth-scroll'],
+    ['seo']
+  ]
+}
